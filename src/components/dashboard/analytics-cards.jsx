cd406f70fef6f84ed11890e6b3f625e3bc266084
FadyEmad01 <fadyemad933@gmail.com>
2026-04-11 20:07:09 +0200

    fix some ui in dashboard

--- src/components/dashboard/analytics-cards.jsx
+++ src/components/dashboard/analytics-cards.jsx
@@ -11,7 +11,7 @@ import { Clock, UserCheck, Users, Activity } from "lucide-react";
 
 export function AnalyticsCards() {
   return (
-    <div className="grid grid-cols-4 gap-4 bg-card">
+    <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 bg-transparent">
       <Card className="@container/card">
         <CardContent className="flex flex-col items-start justify-between gap-4 w-full h-full">
           <CardDescription className="flex justify-between items-center gap-2 w-full">
